b001946ff6147cd04020e0c96b32001f069e84e6
aps1027 <pyaesonefpfsi@gmail.com>
2020-12-29 01:15:33 +0630

    [laravel-eb] change db seeding updateOrInsert

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
-    DB::table('users')->insert(
+    DB::table('users')->updateOrInsert(
       [
         'name' => 'admin',
         'email' => 'pyaesonefpfsi@gmail.com',
